9ca25f96c1115af396ab005a3146aef083b65782
Nathan Faucett <nathanfaucett@gmail.com>
2019-04-14 10:45:00 -0400

    add sudo

--- src/uninstall/rancher.sh
+++ src/uninstall/rancher.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker rm -f $(docker ps -qa)
-docker rmi -f $(docker images -q)
-docker volume rm $(docker volume ls -q)
+sudo docker rm -f $(docker ps -qa)
+sudo docker rmi -f $(docker images -q)
+sudo docker volume rm $(docker volume ls -q)
 
 for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
 
-rm -rf /etc/ceph \
+sudo rm -rf /etc/ceph \
        /etc/cni \
        /etc/kubernetes \
        /opt/cni \
