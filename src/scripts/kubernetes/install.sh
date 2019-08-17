8414e4ed18473764df015ad9462b369d3e45687c
Nathan Faucett <nathanfaucett@gmail.com>
2019-08-17 15:11:36 -0400

    add user to docker group

--- src/scripts/kubernetes/install.sh
+++ src/scripts/kubernetes/install.sh
@@ -23,6 +23,8 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io  -y
 
+sudo usermod -aG docker $USER
+
 sudo snap install microk8s --classic
 
 microk8s.stop
