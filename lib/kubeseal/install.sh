7d3367b385b8aeb2f69fd5188b13726d15ccf606
Nathan Faucett <nathanfaucett@gmail.com>
2020-10-02 19:09:20 -0400

    update kubeseal

--- lib/kubeseal/install.sh
+++ lib/kubeseal/install.sh
@@ -2,10 +2,10 @@
 
 dir=$(readlink -f "$(dirname "$0")")
 
-sealed_secrets_version=0.12.5
+sealed_secrets_version=0.12.6
 
 if ! hash kubeseal 2>/dev/null; then
   wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v${sealed_secrets_version}/kubeseal-linux-amd64 -O kubeseal
-  install -m 755 kubeseal /usr/local/bin/kubeseal
+  sudo install -m 755 kubeseal /usr/local/bin/kubeseal
   rm kubeseal
 fi
